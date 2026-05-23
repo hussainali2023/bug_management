eb0cc3073bbc42e75267ca03262f2036687634f8
hussainali2023 <iamhussainali2@gmail.com>
2026-05-23 18:58:04 +0530

    added global error handler

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,8 @@ dotenv.config()
 const config = {
     db_url: process.env.DB_URL,
     port: process.env.PORT,
-    jwt_secret: process.env.JWT_SECRET
+    jwt_secret: process.env.JWT_SECRET,
+    node_env: process.env.NODE_ENV
 }
 
 export default config;
