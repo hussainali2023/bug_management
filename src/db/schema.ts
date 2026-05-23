4c54c9bf940333c28ec9ea082b873ebfc4c23493
hussainali2023 <iamhussainali2@gmail.com>
2026-05-23 18:21:47 +0530

    added schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,29 @@
 import {pool} from "./index"
 
 export const createSchema = async () => {
-    await pool.query(`
-        
-        
-        `)
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+      created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+  `);
+
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS issues (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      description TEXT NOT NULL,
+      type VARCHAR(255) NOT NULL,
+      status VARCHAR(100) NOT NULL DEFAULT 'open',
+      reporter_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+  `);
 }
